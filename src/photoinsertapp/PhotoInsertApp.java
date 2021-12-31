96dfcb3c7dc7a12ef07cb24a2589b400c72f4e3f
Denis Ignatov <dnoevgen@gmail.com>
2021-12-31 21:37:44 +0300

    InsertPhotoApp

---
+++ src/photoinsertapp/PhotoInsertApp.java
@@ -0,0 +1,15 @@
+
+package photoinsertapp;
+
+/**
+ *
+ * @author Denis
+ */
+public class PhotoInsertApp {
+
+  
+     public static void main(String[] args) {
+        // Сохранение выбранного изображения в базу//Saving the selected image to the demonstration data base on Microsoft SQL Server 2019 Express
+        MySQL.putPhoto("Insert photo title without format(without .jpg/.jpeg etc.","Insert the full path to the file");
+    }
+}
